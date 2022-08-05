3c2b80338661e387a3163a88242c29c3bba3f533
lucy6790 <lrs592102@gmail.com>
2022-08-05 13:55:31 +0100

    Tested the file validation - need to add log

--- fileValidation.py
+++ fileValidation.py
@@ -2,38 +2,49 @@ import csv
 
 cols = 12
 maxVal = 9.9
-
-def validateHeadings(row):
-    return len(row) == 12
+headings = ['"batch_id"', '"timestamp"', '"reading1"', '"reading2"', '"reading3"', '"reading4"',\
+     '"reading5"', '"reading6"', '"reading7"', '"reading8"', '"reading9"', '"reading10"']
 
 def validateFile(path):
-    batches = {}
+    batches = set()
     with open(path, newline = '\n') as csvfile:
         reader=csv.reader(csvfile, delimiter=',', quotechar='|')
         lineCount = 0
         for row in reader:
+            print(row)
+
             #check row has enough columns
             if len(row) != cols:
+                print("Row is incomplete.")
                 return False
 
             if lineCount == 0:
                 #headings
-                if not validateHeadings(row):
-                    return False
+                for i in range(0, cols):
+                    if row[i] != headings[i]:
+                        print(row[i] + " " + headings[i])
+                        return False
             else:
                 #check not duplicate batch id
                 batchId = row[0]
                 if batchId in batches:
+                    print("Repeated batch id.")
                     return False
                 else:
                     batches.add(batchId)
                 
                 #check values are in range
-                for r in range(2, cols):
-                    if r > maxVal:
+                for i in range(2, cols):
+                    try:
+                        if float(row[i]) > maxVal:
+                            print("Value too large.")
+                            return False
+                    except:
+                        #not a number
+                        print("Reading is not a number.")
                         return False
             
             lineCount += 1
-        
 
-validateFile("test.csv")
+    return True
+        
