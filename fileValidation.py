f362d6f3c3919c34365e048d37a0c82241018158
lucy6790 <lrs592102@gmail.com>
2022-08-05 14:03:26 +0100

    Logging functionality added

--- fileValidation.py
+++ fileValidation.py
@@ -1,9 +1,19 @@
 import csv
+import json
 
 cols = 12
 maxVal = 9.9
 headings = ['"batch_id"', '"timestamp"', '"reading1"', '"reading2"', '"reading3"', '"reading4"',\
      '"reading5"', '"reading6"', '"reading7"', '"reading8"', '"reading9"', '"reading10"']
+
+def logError(message, fileName):
+    data = {
+        'file' : fileName,
+        'issue' : message
+    }
+    with open('log.json', 'a') as file:
+        json.dump(data, file)
+
 
 def validateFile(path):
     batches = set()
@@ -15,20 +25,20 @@ def validateFile(path):
 
             #check row has enough columns
             if len(row) != cols:
-                print("Row is incomplete.")
+                logError("Row is incomplete.", path)
                 return False
 
             if lineCount == 0:
                 #headings
                 for i in range(0, cols):
                     if row[i] != headings[i]:
-                        print(row[i] + " " + headings[i])
+                        logError("Headings are incorrect.", path)
                         return False
             else:
                 #check not duplicate batch id
                 batchId = row[0]
                 if batchId in batches:
-                    print("Repeated batch id.")
+                    logError("Repeated batch id.", path)
                     return False
                 else:
                     batches.add(batchId)
@@ -37,11 +47,11 @@ def validateFile(path):
                 for i in range(2, cols):
                     try:
                         if float(row[i]) > maxVal:
-                            print("Value too large.")
+                            logError("Value too large.", path)
                             return False
                     except:
                         #not a number
-                        print("Reading is not a number.")
+                        logError("Reading is not a number.", path)
                         return False
             
             lineCount += 1
