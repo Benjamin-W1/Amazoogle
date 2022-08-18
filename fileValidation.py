e3130dcc43d730b6e8d983d4a347c0ec456791a8
lucy6790 <lrs592102@gmail.com>
2022-08-18 16:40:54 +0100

    Added validation for file names

--- fileValidation.py
+++ fileValidation.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import re
 
 cols = 12
 maxVal = 9.9
@@ -14,14 +15,21 @@ def logError(message, fileName):
     with open('log.json', 'a') as file:
         json.dump(data, file)
 
+def validateFileName(path):
+    x = path.split("/")
+    name = x[len(x) - 1]
+    # must match     MED_DATA_  Y    Y    Y    Y    M    M    D    D    H    H    M    M    S    S     .csv
+    return re.search("MED_DATA_[0-2][0-9][0-9][0-9][0-1][0-9][0-3][0-9][0-2][0-9][0-5][0-9][0-5][0-9]\.csv*", name)
 
 def validateFile(path):
+    if not validateFileName(path):
+        logError("File name is invalid", path)
+        return False
     batches = set()
     with open(path, newline = '\n') as csvfile:
         reader=csv.reader(csvfile, delimiter=',', quotechar='|')
         lineCount = 0
         for row in reader:
-            print(row)
 
             #check row has enough columns
             if len(row) != cols:
