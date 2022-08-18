470ad40c5a2910ba85093c319feb517dbcf81f56
Benjamin-W1 <110542588+Benjamin-W1@users.noreply.github.com>
2022-08-18 17:37:25 +0100

    Update archiver_tool.py

--- archiver_tool.py
+++ archiver_tool.py
@@ -2,15 +2,8 @@ from pathlib import Path
 import pathlib
 import shutil
 
-
-
-#needs .csv
-string = "MED_DATA_20220803153918.csv"
-
-
 def store_file(string):
 
-    #splice filename
     year = string[9:13]
     month = string[13:15]
     day = string[15:17]
@@ -18,17 +11,13 @@ def store_file(string):
     minute = string[19:21]
     second = string[21:23]
 
-    #create path
-    path = pathlib.Path(f'Archive/{year}/{month}/{day}/{hour}/{minute}/{second}')
+
+    path = pathlib.Path(f'Archive/Y{year}/M{month}/D{day}')
     path.mkdir(parents=True, exist_ok=True)
 
-    #change source to where downloaded from ftp
-    source = f'C:/Users/gibcl/Downloads/{string}'
+    source = f'{pathlib.Path().resolve()}/{string}'
     destination = path
 
-    #check file doesnt already exist
-    if not path.is_file():
+    if path.is_file():
 
         shutil.move(source, destination)
-    
-store_file(string)
